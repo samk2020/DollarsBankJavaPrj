6737403771956cf37d1f3ac6734545099652f1ea
samk2020 <44036369+samk2020@users.noreply.github.com>
2022-07-31 19:48:21 -0700

    Add files via upload

---
+++ DollarsBankPrj/src/com/samsdollarsbank/exception/DataEx.java
@@ -0,0 +1,11 @@
+// Exception Handling file for data processing of accounts
+package com.samsdollarsbank.exception;
+
+public class DataEx extends Exception{
+	private static final long serialVersionUID = 1000000000L;
+	public DataEx(String string) {
+		super(string); 
+		
+		
+	}
+}
